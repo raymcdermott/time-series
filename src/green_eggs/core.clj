18405842830952e875a2f4eb744446ae819d68bb
Ray McDermott <ray@mcdermott.be>
2016-02-06 14:05:15 +0100

    :bug: no more atoms!! :art: use channel timeout instead of Thread/sleep

--- src/green_eggs/core.clj
+++ src/green_eggs/core.clj
@@ -1,9 +1,13 @@
 (ns green-eggs.core
   (:require [clojure.core.async
              :as a
-             :refer [>! <! >!! <!! go go-loop chan buffer close! onto-chan]]
+             :refer [>! <! >!! <!! alts! go go-loop chan buffer close! onto-chan put! timeout]]
             [clj-time.core :as t]
             [clj-time.coerce :refer [from-date]]))
+
+; TODO experiment with running his via nashron (JDK8 JS VM) to see how it compares to node
+
+; TODO spilt out the testing code
 
 ;If you were to implement yourself, you could make a `go` that simply pulls from a channel, and adds to another as a
 ;`[timestamp item]` pair, then finally pushes into an unbounded `chan` that has a transducer that filters based on
@@ -19,53 +23,16 @@
    "here or there"
    "anywhere"])
 
-(defn delaying-timestamper [millis]
-  (let [now (t/now)]
-    (Thread/sleep millis)
-    now))
+; TODO change logic to remove the need for this
+(def retention-period (t/seconds 10))
 
-(defn add-timestamp [now-provider item]
-  (let [now (now-provider)]
-    [now item]))
-
-(defn time-stamper
-  [timestamper in]
-  (let [out (chan)]
-    (go-loop []
-      (if-let [item (<! in)]
-        (do
-          (>! out (timestamper item))
-          (recur))
-        (close! out)))
-    out))
-
-; for testing the window
-(def add-timestamp-with-delay (partial add-timestamp (partial delaying-timestamper 500)))
-(def add-timestamp-no-delay (partial add-timestamp t/now))
-
-; separate namespace to hide global
-(def time-windows (atom {}))
-
-; TODO support env var that varies the retention period (0 -> n)
-(def retention-period (t/seconds 5))
-
-(defn assoc-window [window]
-  "Maintain the map of current window(s)"
-  (let [all-windows (swap! time-windows assoc (:id window) window)
-        window-maps (map last all-windows)
-        now (t/now)
-        to-be-closed (filter #(and (t/before? (:to %) now) (false? (:closed %))) window-maps)
+(defn maintain-active-windows [windows]
+  (let [now (t/now)
         retention-boundary (t/minus now retention-period)
-        to-be-dropped (filter #(and (:closed %) (t/before? (:to %) retention-boundary)) window-maps)]
-    (doall (map #(swap! time-windows assoc (:id %) (assoc % :closed true)) to-be-closed))
-    (doall (map #(swap! time-windows dissoc (:id %)) to-be-dropped))
-    @time-windows))
-
-(defn add-window [window]
-  (assoc-window window))
-
-(defn update-window [window]
-  (assoc-window window))
+        retained (filter #(t/after? (:to %) retention-boundary) windows)
+        to-be-closed (filter #(and (t/before? (:to %) now) (false? (:closed %))) windows)
+        closing (map #(assoc % :closed true) to-be-closed)]
+    (concat retained closing)))
 
 (defn within-interval? [from to time]
   {:pre [(t/before? from to)]}
@@ -73,55 +40,71 @@
   (let [interval (t/interval from to)]
     (t/within? interval time)))
 
-(defn put-item-in-window [item]
-  (let [[item-time data] item
-        window-maps (map last @time-windows)
-        matching-windows (filter #(within-interval? (:from %) (:to %) item-time) window-maps)
-        updated-windows (map #(assoc % :items (conj (:items %) data)) matching-windows)]
-    (doall (map #(update-window %) updated-windows))
-    @time-windows))
+(defn add-timed-item-to-windows [timed-item windows]
+  "Add an item to the windows where the time intervals match"
+  (if-let [[time item] timed-item]
+    (let [matching-windows (filter #(within-interval? (:from %) (:to %) time) windows)
+          updated-windows (map #(assoc % :items (conj (:items %) item)) matching-windows)]
+      updated-windows)))
 
-(defn create-window [open-duration slide-interval]
-  "Create a window for n seconds that slides every n seconds"
-  {:pre [(> open-duration 0)
-         (> slide-interval 0)]}
+(defn gen-timed-data [coll]
   (let [out (chan)]
-    (go-loop [start-time false]
-      (let [id (gensym)
-            t0 (or start-time (t/now))
-            t1 (t/plus t0 (t/seconds open-duration))
-            w (assoc {} :id id :from t0 :to t1 :closed false :items [])
-            window-tuple ["Window" w]]
-        (do
-          (>! out window-tuple)
-          (Thread/sleep (* 1000 slide-interval))
-          (recur (and (= open-duration slide-interval) t1)))))
+    (go-loop [item (rand-nth coll)]
+      (do
+        (>! out [(t/now) item])
+        (<! (timeout (rand-int 3000)))
+        (recur (rand-nth coll))))
     out))
 
-(defn window-matching
-  "Allocate items to time windows"
-  [in]
+(defn termination-protocol [run-length]
   (let [out (chan)]
-    (go-loop []
-      (if-let [tuple (<! in)]
+    (go
+      (do
+        (<! (timeout (* 1000 run-length)))
+        (>! out :stop)))
+    out))
+
+(defn gen-window [start-time open-duration]
+  {:pre [(> open-duration 0)]}
+  (let [to-time (t/plus start-time (t/seconds open-duration))]
+    (assoc {} :from start-time :to to-time :closed false :items [])))
+
+(defn create-time-series-windows [open-duration slide-interval]
+  "Create a window for n seconds that slides every n seconds"
+  {:pre [(> open-duration 0) (> slide-interval 0)]}
+  (let [out (chan)]
+    (go-loop [start-time (t/now)]
+      (let [window (gen-window start-time open-duration)]
         (do
-          (let [[elem1 elem2] tuple]
-            (cond
-              (= "Window" elem1) (if-let [windows (add-window elem2)]
-                                   (>! out windows))
+          (>! out window)
+          (<! (timeout (* 1000 slide-interval)))
+          (if (= open-duration slide-interval)
+            (recur (:to window))
+            (recur (t/now))))))
+    out))
 
-              (= org.joda.time.DateTime (type elem1)) (if-let [output (put-item-in-window tuple)]
-                                                        (>! out output))
+(defn time-series-data
+  "Allocate items to time windows"
+  [item-ch window-ch]
+  (let [out (chan)]
+    (go-loop [active-windows ()]
+      (if-let [[data chan] (alts! [item-ch window-ch])]
+        (condp = chan
+          window-ch (if-let [windows (maintain-active-windows (conj active-windows data))]
+                      (do                                   ; put this filter on the consumer!!
+                        (if-let [closed (filter #(:closed %) windows)]
+                          (>! out closed))
+                        (recur windows)))
 
-              :else (>! out tuple))
-            (recur)))
+          item-ch (if-let [windows (add-timed-item-to-windows data active-windows)]
+                    (recur windows)))
         (close! out)))
     out))
 
 
 ; TODO: persist the last processed id on each window boundary
 
-; TODO: allow function to be passed in
+; TODO: make a filter for closed windows only, allow function to be passed
 (defn interval-aggregator [in]
   (let [out (chan)]
     (go-loop []
@@ -142,29 +125,31 @@
         (close! out)))
     out))
 
-(defn simple-printer [in]
+(defn infinite-printer [data-ch]
   (go-loop []
-    (if-let [item (<! in)]
+    (if-let [data (<! data-ch)]
       (do
-        (clojure.pprint/pprint item)
+        (clojure.pprint/pprint data)
         (recur)))))
 
-(comment
+(defn finite-printer [termination-ch data-ch]
+  (go-loop []
+    (if-let [[data chan] (alts! [termination-ch data-ch])]
+      (condp = chan
+        termination-ch (println data)
+        data-ch (do
+                  (clojure.pprint/pprint data)
+                  (recur))))))
 
-  (def in-chan (chan))
+;(def in-chan (chan))
+;(def timestamped-data (time-stamper add-timestamp-with-delay in-chan))
+;(def windows-channel (create-time-series-windows 2 2))
+;(def windowed (time-series-data timestamped-data windows-channel))
+;(def aggregated (interval-aggregator windowed))
 
-  (def standard-channel (time-stamper add-timestamp-with-delay in-chan))
 
-  (def windows-channel (create-window 2 2))
-
-  (def merged-channel (a/merge [standard-channel windows-channel]))
-
-  (def windowed (window-matching merged-channel))
-
-  (def aggregated (interval-aggregator windowed))
-
-  (simple-printer windowed)
-
-  (onto-chan in-chan green-eggs-n-ham)
-
-  )
+;(def window-chan (create-time-series-windows 3 3))
+;(def data-chan (gen-timed-data green-eggs-n-ham))
+;(def termination-chan (termination-protocol 120))
+;(def time-series-chan (time-series-data data-chan window-chan))
+;(finite-printer termination-chan time-series-chan)
