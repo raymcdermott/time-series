bb33aa3c844dae2c19947ce4fb2e80c2bb58b042
Ray McDermott <ray@mcdermott.be>
2016-02-06 18:07:55 +0100

    :bug: seems to be alive

--- src/green_eggs/core.clj
+++ src/green_eggs/core.clj
@@ -1,30 +1,16 @@
 (ns green-eggs.core
   (:require [clojure.core.async
-             :as a
+             :as async
              :refer [>! <! >!! <!! alts! go go-loop chan buffer close! onto-chan put! timeout]]
             [clj-time.core :as t]
             [clj-time.coerce :refer [from-date]]))
-
-; TODO experiment with running his via nashron (JDK8 JS VM) to see how it compares to node
-
-; TODO spilt out the testing code
 
 ;If you were to implement yourself, you could make a `go` that simply pulls from a channel, and adds to another as a
 ;`[timestamp item]` pair, then finally pushes into an unbounded `chan` that has a transducer that filters based on
 ; age of that timestamp
 
-(def green-eggs-n-ham
-  ["in the rain"
-   "on a train"
-   "in a box"
-   "with a fox"
-   "in a house"
-   "with a mouse"
-   "here or there"
-   "anywhere"])
-
 ; TODO change logic to remove the need for this
-(def retention-period (t/seconds 10))
+(def retention-period (t/millis 750))
 
 (defn maintain-active-windows [windows]
   (let [now (t/now)
@@ -52,7 +38,7 @@
     (go-loop [item (rand-nth coll)]
       (do
         (>! out [(t/now) item])
-        (<! (timeout (rand-int 3000)))
+        (<! (timeout (rand-int 1000)))
         (recur (rand-nth coll))))
     out))
 
@@ -70,8 +56,8 @@
     (assoc {} :from start-time :to to-time :closed false :items [])))
 
 (defn create-time-series-windows [open-duration slide-interval]
-  "Create a window for n seconds that slides every n seconds"
-  {:pre [(> open-duration 0) (> slide-interval 0)]}
+  "Create a window for X seconds that slides every Y seconds. 1 <= Y <= X"
+  {:pre [(> open-duration 0) (> slide-interval 0) (>= open-duration slide-interval)]}
   (let [out (chan)]
     (go-loop [start-time (t/now)]
       (let [window (gen-window start-time open-duration)]
@@ -84,72 +70,20 @@
     out))
 
 (defn time-series-data
-  "Allocate items to time windows"
+  "Add timed data from item-ch to the time series windows produced in the window-ch"
   [item-ch window-ch]
-  (let [out (chan)]
+  (let [out-ch (chan)]
     (go-loop [active-windows ()]
       (if-let [[data chan] (alts! [item-ch window-ch])]
         (condp = chan
           window-ch (if-let [windows (maintain-active-windows (conj active-windows data))]
-                      (do                                   ; put this filter on the consumer!!
-                        (if-let [closed (filter #(:closed %) windows)]
-                          (>! out closed))
+                      (do
+                        (let [closed (filter #(:closed %) windows)]
+                          (if (not (empty? closed))
+                            (>! out-ch closed)))
                         (recur windows)))
 
           item-ch (if-let [windows (add-timed-item-to-windows data active-windows)]
                     (recur windows)))
-        (close! out)))
-    out))
-
-
-; TODO: persist the last processed id on each window boundary
-
-; TODO: make a filter for closed windows only, allow function to be passed
-(defn interval-aggregator [in]
-  (let [out (chan)]
-    (go-loop []
-      (if-let [windows (<! in)]
-        (do
-          (if-let [totals (remove nil? (map (fn [window]
-                                              (let [map-data (last window)
-                                                    items (:items map-data)]
-                                                (if (:closed map-data)
-                                                  (if-let [total (reduce (fn [result item]
-                                                                           (let [c (count item)]
-                                                                             (+ result c))) 0 items)]
-                                                    [total (:id map-data)]))))
-                                            windows))]
-            (if (not (empty? totals))
-              (>! out totals)))
-          (recur))
-        (close! out)))
-    out))
-
-(defn infinite-printer [data-ch]
-  (go-loop []
-    (if-let [data (<! data-ch)]
-      (do
-        (clojure.pprint/pprint data)
-        (recur)))))
-
-(defn finite-printer [termination-ch data-ch]
-  (go-loop []
-    (if-let [[data chan] (alts! [termination-ch data-ch])]
-      (condp = chan
-        termination-ch (println data)
-        data-ch (do
-                  (clojure.pprint/pprint data)
-                  (recur))))))
-
-;(def in-chan (chan))
-;(def timestamped-data (time-stamper add-timestamp-with-delay in-chan))
-;(def windows-channel (create-time-series-windows 2 2))
-;(def windowed (time-series-data timestamped-data windows-channel))
-;(def aggregated (interval-aggregator windowed))
-
-
-;(def window-chan (create-time-series-windows 3 3))
-;(def data-chan (gen-timed-data green-eggs-n-ham))
-;(def termination-chan (termination-protocol 120))
-;(def time-series-chan (time-series-data data-chan window-chan))
-;(finite-printer termination-chan time-series-chan)
+        (close! out-ch)))
+    out-ch))
--- test/green_eggs/core_test.clj
+++ test/green_eggs/core_test.clj
@@ -1,7 +1,86 @@
 (ns green-eggs.core-test
   (:require [clojure.test :refer :all]
-            [green-eggs.core :refer :all]))
+            [clojure.core.async
+             :as async
+             :refer [>! <! >!! <!! alts! go go-loop chan buffer close! onto-chan put! timeout]]
+            [green-eggs.core :refer :all]
+            [clj-time.core :as t]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(def green-eggs-n-ham
+  ["in the rain"
+   "on a train this one will skew the averages"
+   "in a box"
+   "with a fox"
+   "in a house or x y z k"
+   "with a mouse"
+   "here or there"
+   "anywhere"])
+
+(defn char-count [words]
+  (reduce (fn [result item]
+            (let [c (count item)]
+              (+ result c))) 0 words))
+
+(defn distinct-letter-count [words]
+  (count (set (apply str words))))
+
+(defn interval-aggregator [aggregator in]
+  "Execute the provided aggregator against the :items property from the maps on the channel"
+  (let [out (chan)]
+    (go-loop []
+             (if-let [windows (<! in)]
+               (do
+                 (let [results (map #(aggregator (:items %)) windows)]
+                   (if (= 1 (count results))
+                     (>! out (first results))
+                     (>! out results))
+                   (recur)))
+               (close! out)))
+    out))
+
+(def distinct-letter-aggregations (partial interval-aggregator distinct-letter-count))
+(def char-count-aggregations (partial interval-aggregator char-count))
+
+(defn infinite-printer [data-ch]
+  (let [test-ch (chan)]
+    (go-loop []
+      (if-let [data (<! data-ch)]
+        (do
+          (clojure.pprint/pprint data)
+          (>! test-ch data)
+          (recur))))))
+
+(defn finite-printer [termination-ch data-ch]
+  (let [test-ch (chan)]
+    (go-loop []
+      (if-let [[data chan] (alts! [termination-ch data-ch])]
+        (condp = chan
+          termination-ch (println data)
+          data-ch (do
+                    (clojure.pprint/pprint data)
+                    (>! test-ch data)
+                    (recur)))))))
+
+;(def window-chan (create-time-series-windows 3 3))
+;(def data-chan (gen-timed-data green-eggs-n-ham))
+;(def termination-chan (termination-protocol 10))
+;(def time-series-chan (time-series-data data-chan window-chan))
+;(def aggregated (distinct-letter-aggregations time-series-chan))
+;(finite-printer termination-chan aggregated)
+
+;(def window-chan (create-time-series-windows 4 2))
+;(def data-chan (gen-timed-data green-eggs-n-ham))
+;(def termination-chan (termination-protocol 30))
+;(def time-series-chan (time-series-data data-chan window-chan))
+;(def aggregated (char-count-aggregations time-series-chan))
+;(finite-printer termination-chan aggregated)
+
+;(def window-chan (create-time-series-windows 3 3))
+;(def data-chan (gen-timed-data green-eggs-n-ham))
+;(def termination-chan (termination-protocol 120))
+;(def time-series-chan (time-series-data data-chan window-chan))
+;(finite-printer termination-chan time-series-chan)
